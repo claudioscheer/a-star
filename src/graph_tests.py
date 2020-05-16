eaffe560fb324bca3e84ba60e34b7ae85401c24f
Claudio Scheer <claudioscheer@protonmail.com>
2020-05-16 01:09:43 -0300

    Create a graph for testing

---
+++ src/graph_tests.py
@@ -0,0 +1,16 @@
+from lib import Graph, Node
+
+
+def get_graph_1():
+    graph = Graph()
+    node_1 = Node(1)
+    node_2 = Node(2)
+    node_3 = Node(3)
+
+    graph.set_bidirectional_neighbor(node_1, node_2, 1)
+    graph.set_bidirectional_neighbor(node_1, node_3, 1)
+    # graph.set_bidirectional_neighbor(node_1, node_2, 1)
+
+    graph.add_nodes([node_1, node_2, node_3])
+
+    return graph, node_1, node_3
