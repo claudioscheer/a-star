75cc27334ef75039b8fa1c393ec4976a50a91da1
Claudio Scheer <claudioscheer@protonmail.com>
2020-05-19 01:07:24 -0300

    Correction in maximum cost heuristic

--- src/graph_tests.py
+++ src/graph_tests.py
@@ -3,21 +3,19 @@ from lib import Graph, Node
 
 def get_graph_1():
     graph = Graph()
-    node_1 = Node(1)
-    node_2 = Node(2)
-    node_3 = Node(3)
-    node_4 = Node(4)
-    node_5 = Node(5)
-    node_6 = Node(6)
-    node_7 = Node(7)
+    node_a = Node(1)
+    node_b = Node(2)
+    node_c = Node(3)
+    node_d = Node(4)
+    node_e = Node(5)
 
-    graph.set_bidirectional_neighbor(node_1, node_2, 1)
-    graph.set_bidirectional_neighbor(node_1, node_3, 1)
-    graph.set_bidirectional_neighbor(node_2, node_4, 1)
-    graph.set_bidirectional_neighbor(node_3, node_4, 1)
-    graph.set_bidirectional_neighbor(node_3, node_5, 1)
-    graph.set_bidirectional_neighbor(node_5, node_6, 1)
+    graph.set_bidirectional_neighbor(node_a, node_b, 24)
+    graph.set_bidirectional_neighbor(node_b, node_e, 15)
 
-    graph.add_nodes([node_1, node_2, node_3, node_4, node_5, node_6, node_7])
+    graph.set_bidirectional_neighbor(node_a, node_c, 15)
+    graph.set_bidirectional_neighbor(node_c, node_d, 14)
+    graph.set_bidirectional_neighbor(node_d, node_e, 10)
 
-    return graph, node_1, node_7
+    graph.add_nodes([node_a, node_b, node_c, node_d, node_e])
+
+    return graph, node_a, node_e
--- src/heuristics.py
+++ src/heuristics.py
@@ -3,7 +3,9 @@ def max_cost(start, goal):
     goal = frozenset([goal])
     max_cost = 0
     while not goal.issubset(reachable):
-        new_reachable = reachable.union([neighbor for neighbor, _ in start.neighbors])
+        new_reachable = reachable.union(
+            [neighbor for node in reachable for neighbor, _ in node.neighbors]
+        )
         if new_reachable == reachable:
             return float("inf")
         reachable = new_reachable
